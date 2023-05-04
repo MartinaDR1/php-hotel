2d623fc043ba3ca0e4fe2dae721c4165780e7b20
MartinaDR1 <derosemartina933@gmail.com>
2023-05-04 16:44:57 +0200

    add table

--- index.php
+++ index.php
@@ -49,15 +49,52 @@ $hotels = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <title> PHP Hotel</title>
 </head>
 <body>
-    <?php foreach ($hotels as $hotel): ?>
-        <h3><?=$hotel['name']?></h3>
-        <p><?=$hotel['description']?></p>
-        <p><?=$hotel['parking']?></p>
-        <p><?=$hotel['vote']?></p>
-        <p><?=$hotel['distance_to_center']?></p>
-    <?php endforeach;?>
+
+    <h1 class="text-center pb-5">Hotels</h1>
+    <table class="table text-center">
+        <thead>
+            <tr>
+                <th scope="col">Hotel name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Parking</th>
+                <th scope="col">Vote</th>
+                <th scope="col">Distance to center</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php foreach ($hotels as $hotel): ?>
+            <tr>
+                <th scope="row">
+                    <?=$hotel['name']?>
+                </th>
+                <td>
+                    <?=$hotel['description']?>
+                </td>
+                <td>
+                    <?php if ($hotel["parking"] === true) { ?>
+                        <i class="fa-regular fa-circle-check text-success"></i>
+                    <?php } else { ?>
+                        <i class="fa-regular fa-circle-xmark text-danger"></i>
+                    <?php } ?>
+                </td>
+                <td>
+                    <?=$hotel['vote']?>
+                </td>
+                <td>
+                    <?=$hotel['distance_to_center']?>
+                </td>
+            </tr>
+        </tbody>
+        <?php endforeach;?>
+    </table>
+
 </body>
 </html>
